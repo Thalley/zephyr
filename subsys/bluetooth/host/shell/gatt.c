dcfef4a80ca61a9f54b90236d29d1b0f231a47cd
Emil Gydesen <emil.gydesen@nordicsemi.no>
2026-09-21 15:27:56 +0200

    Bluetooth: GATT: Shell: Add checks for notification lenghts
    Add checks for notification lenghts in the GATT shell, and
    truncate long notifications.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- subsys/bluetooth/host/shell/gatt.c
+++ subsys/bluetooth/host/shell/gatt.c
@@ -614,6 +614,11 @@ static uint8_t notify_func(struct bt_conn *conn,
 		       params->value_handle, length);
 	bt_shell_hexdump(data, length);
 
+	if (length > 0U && length == bt_att_get_max_ntf_size(conn)) {
+		bt_shell_print("Value fills a notification, it may have been truncated. Read the "
+			       "characteristic to get the complete value.");
+	}
+
 	return BT_GATT_ITER_CONTINUE;
 }
 
@@ -829,20 +834,32 @@ static const struct bt_uuid_128 vnd1_uuid = BT_UUID_INIT_128(
 static const struct bt_uuid_128 vnd1_echo_uuid = BT_UUID_INIT_128(
 	BT_UUID_128_ENCODE(0x12345678, 0x1234, 0x5678, 0x12340, 0x56789abcdef5));
 
-static uint8_t echo_enabled;
-
-static void vnd1_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value)
-{
-	echo_enabled = (value == BT_GATT_CCC_NOTIFY) ? 1 : 0;
-}
-
 static ssize_t write_vnd1(struct bt_conn *conn, const struct bt_gatt_attr *attr,
 			  const void *buf, uint16_t len, uint16_t offset,
 			  uint8_t flags)
 {
-	if (echo_enabled) {
+	const uint16_t max_ntf_size = bt_att_get_max_ntf_size(conn);
+	const uint16_t ntf_len = MIN(len, max_ntf_size);
+	int err;
+
+	if (max_ntf_size == 0U) {
+		/* Not connected */
+		return len;
+	}
+
+	if (!bt_gatt_is_subscribed(conn, attr, BT_GATT_CCC_NOTIFY)) {
+		return len;
+	}
+
+	if (ntf_len < len) {
+		bt_shell_print("Echo attr len %u truncated to %u", len, ntf_len);
+	} else {
 		bt_shell_print("Echo attr len %u", len);
-		bt_gatt_notify(conn, attr, buf, len);
+	}
+
+	err = bt_gatt_notify(conn, attr, buf, ntf_len);
+	if (err != 0) {
+		bt_shell_error("Failed to notify echo: %d", err);
 	}
 
 	return len;
@@ -939,8 +956,7 @@ static struct bt_gatt_attr vnd1_attrs[] = {
 			       BT_GATT_CHRC_WRITE_WITHOUT_RESP |
 			       BT_GATT_CHRC_NOTIFY,
 			       BT_GATT_PERM_WRITE, NULL, write_vnd1, NULL),
-	BT_GATT_CCC(vnd1_ccc_cfg_changed,
-		    BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
+	BT_GATT_CCC(NULL, BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
 };
 
 static struct bt_gatt_service vnd1_svc = BT_GATT_SERVICE(vnd1_attrs);
@@ -1014,9 +1030,48 @@ static const struct bt_gatt_attr *find_attr(uint16_t handle)
 	return attr;
 }
 
+struct notify_conn_data {
+	const struct shell *sh;
+	const struct bt_gatt_attr *attr;
+	const char *data;
+	size_t data_len;
+};
+
+static void notify_conn_cb(struct bt_conn *conn, void *user_data)
+{
+	struct notify_conn_data *notify_data = user_data;
+	const uint16_t max_ntf_size = bt_att_get_max_ntf_size(conn);
+	size_t data_len = notify_data->data_len;
+	int err;
+
+	if (max_ntf_size == 0U) {
+		/* Not connected */
+		return;
+	}
+
+	if (!bt_gatt_is_subscribed(conn, notify_data->attr, BT_GATT_CCC_NOTIFY)) {
+		return;
+	}
+
+	if (data_len > max_ntf_size) {
+		shell_print(notify_data->sh,
+			    "Truncating notification to conn %p from %zu to %u octets.", conn,
+			    data_len, max_ntf_size);
+		data_len = max_ntf_size;
+	}
+
+	err = bt_gatt_notify(conn, notify_data->attr, notify_data->data, data_len);
+	if (err != 0 && err != -ENOTCONN) {
+		/* -ENOTCONN means that the connection has not subscribed */
+		shell_error(notify_data->sh, "bt_gatt_notify to conn %p errno %d (%s)", conn, -err,
+			    strerror(-err));
+	}
+}
+
 static int cmd_notify(const struct shell *sh, size_t argc, char *argv[])
 {
 	const struct bt_gatt_attr *attr;
+	struct notify_conn_data notify_data;
 	int err;
 	size_t data_len;
 	unsigned long handle;
@@ -1055,12 +1110,18 @@ static int cmd_notify(const struct shell *sh, size_t argc, char *argv[])
 		return -EINVAL;
 	}
 
-	err = bt_gatt_notify(NULL, attr, data, data_len);
-	if (err) {
-		shell_error(sh, "bt_gatt_notify errno %d (%s)", -err, strerror(-err));
-	}
+	/* The notification goes to every subscriber, which may have negotiated different ATT
+	 * MTUs, so notify each connection individually to be able to truncate the value to what
+	 * that connection can carry.
+	 */
+	notify_data.sh = sh;
+	notify_data.attr = attr;
+	notify_data.data = data;
+	notify_data.data_len = data_len;
 
-	return err;
+	bt_conn_foreach(BT_CONN_TYPE_LE, notify_conn_cb, &notify_data);
+
+	return 0;
 }
 
 #if defined(CONFIG_BT_GATT_NOTIFY_MULTIPLE)
@@ -1086,9 +1147,8 @@ static int cmd_notify_mult(const struct shell *sh, size_t argc, char *argv[])
 		return -ENOEXEC;
 	}
 
-	if (!echo_enabled) {
+	if (!bt_gatt_is_subscribed(conn, vnd1_attrs, BT_GATT_CCC_NOTIFY)) {
 		shell_error(sh, "No clients have enabled notifications for the vnd1_echo CCC.");
-
 		return -ENOEXEC;
 	}
 
